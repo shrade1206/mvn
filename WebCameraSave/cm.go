9a3819751d451d8816f92b5b41fcbcb80567931a
shrade1206 <88537622+shrade1206@users.noreply.github.com>
2021-10-14 11:55:30 +0800

    Add files via upload

--- WebCameraSave/cm.go
+++ WebCameraSave/cm.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"log"
 	"os"
+	"time"
 
 	"gocv.io/x/gocv"
 )
@@ -13,21 +15,22 @@ func main() {
 	saveFile := os.Args[1]
 
 	// 設定視訊設備
-	webcam, _ := gocv.VideoCaptureDevice(0)
+	webcam, err := gocv.VideoCaptureDevice(0)
+	if err != nil {
+		log.Println(err)
+	}
 
 	defer webcam.Close()
-	// 設定視窗跟名稱
-	window := gocv.NewWindow("Hello")
+	// 延長視訊鏡頭時間 1s，以免曝光太短
+	time.Sleep(time.Second)
+
 	// 設定一個框，拿來儲存畫面
 	img := gocv.NewMat()
 
 	defer img.Close()
 	// 設備讀到畫面
 	webcam.Read(&img)
-	// 在指定的地方顯示圖片
-	window.IMShow(img)
 
-	defer window.Close()
 	// 保存設定的名字與擷取的照片
 	gocv.IMWrite(saveFile, img)
 }
